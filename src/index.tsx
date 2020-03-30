6e95a8fcc6a5e6db3b79f328c6afed8f534dcb85
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2020-03-30 21:02:28 +0100

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,33 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/a">A</Link>
+          </li>
+        </ul>
+
+      <Switch>
+        <Route path="/a" component={App} />
+        <Route path="*">
+          <Error/>
+        </Route>
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
+function Error() {
+  return <h2>Error</h2>;
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
